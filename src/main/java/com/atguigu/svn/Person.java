3b5bdc3b168014a8711b3e3c2d83e5b8c26465b4
Ricardo <1783015395@qq.com>
2018-10-16 19:38:19 +0800

    hotfix分支进行推送

--- src/main/java/com/atguigu/svn/Person.java
+++ src/main/java/com/atguigu/svn/Person.java
@@ -5,7 +5,7 @@ public class Person {
 	public static void main(String[] args) {
 		System.out.println("Aaa...");
 		
-		System.out.println("Zzzzzzzz");//right
+		System.out.println("hot_fix...");
 	}
 
 }
